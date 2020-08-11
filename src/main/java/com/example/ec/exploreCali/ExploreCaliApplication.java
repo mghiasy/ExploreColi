e794854d614510cd37bc601ff406552c5ffdf296
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-08-10 21:33:00 -0500

    Add ExploreColi Project

---
+++ src/main/java/com/example/ec/exploreCali/ExploreCaliApplication.java
@@ -0,0 +1,13 @@
+package com.example.ec.exploreCali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreCaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreCaliApplication.class, args);
+	}
+
+}
